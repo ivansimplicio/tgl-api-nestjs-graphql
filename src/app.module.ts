704f96a5e501609d2e6cf60f7dbb3f086afab230
ivansimplicio <ivansimplicio@outlook.com>
2022-03-05 10:57:53 -0300

    feat: GraphQL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
   ],
   controllers: [],
   providers: [],
